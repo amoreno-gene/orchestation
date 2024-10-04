2acd2df41fc093702994e3354f4f08a8c9069999
Alejandro Moreno <alejmo12@ucm.com>
2024-10-04 15:37:40 +0200

    configuracion conexion prueba snowflake

---
+++ dags/integrators/snowflake_conn_test.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.models import Variable
+
+# Definir los parámetros básicos del DAG
+default_args = {
+    'start_date': days_ago(1),
+    'catchup': False
+}
+
+# DAG para probar la conexión a Snowflake
+with DAG(
+    dag_id='snowflake_test_connection',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    max_active_runs=1,
+    description='Test connection to Snowflake'
+) as dag:
+
+    # Operador para ejecutar una consulta SQL en Snowflake
+    run_snowflake_query = SnowflakeOperator(
+        task_id='run_snowflake_query',
+        snowflake_conn_id='my_snowflake_conn',  # Asegúrate de que esta conexión está configurada en Airflow
+        sql="SELECT CURRENT_VERSION();"
+    )
+
+    run_snowflake_query
+
