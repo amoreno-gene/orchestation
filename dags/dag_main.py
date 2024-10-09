23594f6d102ae80175d0d94c561c4e385ba801be
Alejandro Moreno <alejmo12@ucm.com>
2024-10-09 11:09:23 +0200

    cambios para extraer carpetas dinamicas con casos de uso

--- dags/dag_main.py
+++ dags/dag_main.py
@@ -16,15 +16,20 @@ logger = logging.getLogger(__name__)
 # Definir parámetros
 SNOWFLAKE_CONN_ID = 'Snowflake_orchestation_metadata_schema_conn'
 GCS_BUCKET_NAME = "intermediate-datalake"
-ORQUESTADOR_ID = 1  # ID del orquestador que estás ejecutando
+ORQUESTADOR_ID = 1  
 
 # Consulta SQL para obtener los orígenes activos
 SNOWFLAKE_ORIGINS_QUERY = f"""
-    SELECT o.id_origen, o.nombre_origen
-    FROM origenes_orquestadores oo
-    JOIN origenes o ON oo.id_origen = o.id_origen
-    WHERE oo.id_orquestador = {ORQUESTADOR_ID}
-    AND o.activo = TRUE;
+    SELECT 
+        o.id_origen, o.nombre_origen, cu.nombre AS nombre_caso_uso, cu.area_negocio
+    FROM    origenes_orquestadores oo
+        JOIN origenes o ON oo.id_origen = o.id_origen
+        JOIN casos_uso cu ON o.id_caso_uso = cu.id_caso_uso
+    WHERE 
+        oo.id_orquestador = {ORQUESTADOR_ID}
+        AND o.activo = TRUE
+        AND cu.activo = TRUE;
+
 """
 
 # Función para importar dinámicamente un módulo de un archivo
@@ -43,7 +48,7 @@ def import_module_from_path(module_name, file_path):
 def dag_main_orquestador_uno():
     t0 = DummyOperator(task_id="start")
 
-    # Tarea para consultar orígenes activos en Snowflake
+    # Tarea para consultar orígenes activos y el caso de uso en Snowflake
     @task(task_id="get_active_origins")
     def get_active_origins():
         hook = SnowflakeHook(snowflake_conn_id=SNOWFLAKE_CONN_ID)
@@ -56,16 +61,15 @@ def dag_main_orquestador_uno():
         logger.info(f"Orígenes obtenidos: {result}")
         return result
 
-    # Tarea para extraer y subir datos de orígenes activos
     @task(task_id="extract_and_upload_dynamic", multiple_outputs=True)
     def extract_and_upload_dynamic(origins):
         for origin in origins:
-            id_origen, nombre_origen = origin
-            logger.info(f"Extrayendo datos para el origen: {nombre_origen} (ID: {id_origen})")
+            id_origen, nombre_origen, nombre_caso_uso, area_negocio = origin
+            logger.info(f"Extrayendo datos para el origen: {nombre_origen} (ID: {id_origen}), Caso de uso: {nombre_caso_uso}, Área de negocio: {area_negocio}")
             
-            # Ruta del script de extracción correspondiente
+            # Ruta del script de extracción correspondiente dentro de la carpeta del área de negocio y caso de uso
             extractor_script = f"extract_{nombre_origen.lower()}.py"
-            extractor_path = os.path.join("extractors", extractor_script)
+            extractor_path = os.path.join(f"{area_negocio}/{nombre_caso_uso}/extractors", extractor_script)
             
             # Verificar si el script existe
             if os.path.exists(extractor_path):
@@ -73,11 +77,12 @@ def dag_main_orquestador_uno():
                 csv_file = module.extract_and_process_data()  # Llamada a la función de extracción
                 timestamp = dt.now().strftime("%Y%m%d%H%M%S")
                 filename = f"{os.path.splitext(os.path.basename(csv_file))[0]}_{timestamp}.csv"
-                folder_path = f"orquestador_{ORQUESTADOR_ID}/origen_{id_origen}"
+                folder_path = f"{area_negocio}/orquestador_{ORQUESTADOR_ID}/origen_{id_origen}/caso_uso_{nombre_caso_uso}"
                 upload_to_gcs(csv_file, filename, folder_path)  # Subida del archivo a GCS
-                logger.info(f"Datos extraídos y subidos para {nombre_origen}")
+                logger.info(f"Datos extraídos y subidos para {nombre_origen}, Caso de uso {nombre_caso_uso} en área {area_negocio}")
             else:
-                logger.error(f"No se encontró el script de extracción para {nombre_origen}")
+                logger.error(f"No se encontró el script de extracción para {nombre_origen} en el caso de uso {nombre_caso_uso} y área de negocio {area_negocio}")
+
 
     # Definir flujo de tareas
     origins = get_active_origins()
